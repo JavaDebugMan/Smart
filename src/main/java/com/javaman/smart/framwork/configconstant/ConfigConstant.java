a02402ac51f0778dca2ed33201e6fd6ad8ca4571
pengzhe <pengzhepost@sina.com>
2018-02-26 23:25:22 +0800

    init smart

---
+++ src/main/java/com/javaman/smart/framwork/configconstant/ConfigConstant.java
@@ -0,0 +1,22 @@
+package com.javaman.smart.framwork.configconstant;
+
+/**
+ * @author pengzhe
+ * @date 2018/2/26 23:07
+ * @description 提供相关配置项常量
+ */
+
+public interface ConfigConstant {
+
+    String CONFIG_FILE = "smart.properties";
+
+    String JDBC_DRIVER = "smart.framwork.jdbc.driver";
+    String JDBC_URL = "smart.framwork.jdbc.url";
+    String JDBC_USERNAME = "smart.framwork.jdbc.username";
+    String JDBC_PASSWORD = "smart.framwork.jdbc.password";
+
+    String APP_BASE_PACKAGE = "smart.framwork.app.base_package";
+    String APP_JSP_PATH = "smart.framwork.app.jsp_path";
+    String APP_ASSET_PATH = "smart.framwork.app.asset_path";
+
+}

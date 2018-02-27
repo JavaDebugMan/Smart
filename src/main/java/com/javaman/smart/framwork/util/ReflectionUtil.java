c0a4ff22327bfd99a19a21e916f26a93a0e60af0
pengzhe <pengzhepost@sina.com>
2018-02-27 09:55:49 +0800

    新增反射工具类

---
+++ src/main/java/com/javaman/smart/framwork/util/ReflectionUtil.java
@@ -0,0 +1,73 @@
+package com.javaman.smart.framwork.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * @author pengzhe
+ * @date 2018/2/27 09:46
+ * @description 反射工具类
+ */
+
+public class ReflectionUtil {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /**
+     * 创建实例
+     *
+     * @param clazz
+     * @return
+     */
+    public static Object newInstance(Class<?> clazz) {
+        Object instance;
+        try {
+            instance = clazz.newInstance();
+        } catch (Exception e) {
+            LOGGER.error("new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+
+    /**
+     * 调用方法
+     *
+     * @param object
+     * @param method
+     * @param args
+     * @return
+     */
+    public static Object invokeMethod(Object object, Method method, Object... args) {
+        Object result = null;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(object, args);
+        } catch (Exception e) {
+            LOGGER.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置成员变量的值
+     *
+     * @param object
+     * @param field
+     * @param value
+     */
+    public static void setFeild(Object object, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (IllegalAccessException e) {
+            LOGGER.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
+}
